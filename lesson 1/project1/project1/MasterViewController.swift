28f3b5d4f8f239f9a8bbda5108876aeb9738f0a2
Joel Abitan <shalomabitan@gmail.com>
2016-02-18 10:25:11 -0800

    starting with tutorials

---
+++ lesson 1/project1/project1/MasterViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MasterViewController.swift
+//  project1
+//
+//  Created by Shalom Abitan on 2/18/16.
+//  Copyright © 2016 Reliable Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //get the defult manager
+        let fm = NSFileManager.defaultManager()
+        //get the resource path - ! means we will for sure get a string
+        let path = NSBundle.mainBundle().resourcePath!
+        //set the items to be an array of all the paths current files
+        let items = try! fm.contentsOfDirectoryAtPath(path)
+        
+        //for each item in items
+        for item in items {
+            //if the item has a prefix of "nssl"
+            if item.hasPrefix("nssl") {
+                //then push onto the objects array the item
+                objects.append(item)
+            }
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+
+
+}
+
